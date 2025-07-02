2fa51101239ec74b72a61f7a46412811a789e300
Pertinate <ppertinate@gmail.com>
2025-07-01 23:53:39 -0500

    feat: restructure styles and update layout for Onyx theme integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-// import '~/styles/globals.css';
-import './globals.css';
+import type React from 'react';
+import '~/styles/globals.css';
+// import './globals.css';
 import { ThemeProvider } from '~/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
